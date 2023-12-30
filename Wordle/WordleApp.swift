6ac545057052275196694182745265850d392f6e
Kranti <darkphoton20@gmail.com>
2023-12-30 19:36:53 +0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Kranti on 2023/12/30.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
